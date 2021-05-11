8837a08a0c1a62def1a9d024dbfad3e25f4fc103
David Brady <davidjbrady97@gmail.com>
2021-05-10 18:15:23 -0700

    incomplete - step 4, z-indexes not working

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import PerspectiveBackground from './components/PerspectiveBackground';
 
 function App() {
   return (
     <>
       <CharacterEditor />
       <Footer />
+      <PerspectiveBackground />
     </>
   );
 }
---
+++ src/components/PerspectiveBackground/PerspectiveBackground.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './PerspectiveBackground.module.css';
+
+function PerspectiveBackground() {
+    return ( <div className ={styles.thicc} ></div>);
+    };
+
+export default PerspectiveBackground;
